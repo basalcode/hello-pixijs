86d801250959486c1ec13615c2de837ee6a1a36b
basalcode <basalcode@gmail.com>
2022-01-07 23:04:27 +0900

    Create graphics and text

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Application, Container, Sprite } from 'pixi.js'
+import { Application, Container, Graphics, Sprite, TextStyle, Text } from 'pixi.js'
 
 const app = new Application({
 	view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
@@ -15,6 +15,7 @@ container.y = 0;
 
 app.stage.addChild(container);
 
+// prite to show bitmap image
 const clampy: Sprite = Sprite.from("clampy.png");
 
 clampy.x  = 100;
@@ -30,3 +31,32 @@ app.stage.addChild(bigContainer);
 
 const littleContainer: Container = new Container();
 bigContainer.addChild(littleContainer);
+
+// fast speed container
+// const particleContainer: ParticleContainer = new ParticleContainer();
+
+// support drawing 
+const graphics: Graphics = new Graphics();
+
+graphics.beginFill(0xFF00FF);
+graphics.lineStyle(10, 0x00FF00);
+graphics.drawCircle(0, 0, 25);
+graphics.endFill();
+
+app.stage.addChild(graphics);
+
+graphics.x = 100;
+graphics.y = 100;
+
+
+// text
+const textStyle: TextStyle = new TextStyle({
+	align: "center",
+	fill: "#754c24",
+	fontSize: 42,
+});
+
+const textObj: Text = new Text ('ㄱㄴㄷㄹ', textStyle);
+textObj.text = "This is expensive to change, please do not abuse";
+
+app.stage.addChild(textObj);
